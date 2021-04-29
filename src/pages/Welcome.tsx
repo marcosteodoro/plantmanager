de2a80448b4f68a4ea83cc17af3cc9a381e80b40
Marcos Teodoro <marcosvinici_us@live.com>
2021-04-29 13:23:59 -0300

    Refatora tela de Welcome

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -5,10 +5,10 @@ import {
   Image,
   TouchableOpacity,
   StyleSheet,
+  Dimensions,
 } from 'react-native'
 
 import wateringImg from '../assets/watering.png'
-import { Button } from '../components/Button'
 import colors from '../styles/colors'
 
 export function Welcome() {
@@ -20,14 +20,20 @@ export function Welcome() {
         de forma fácil
       </Text>
 
-      <Image source={wateringImg} style={styles.image} />
+      <Image source={wateringImg} style={styles.image} resizeMode="contain" />
 
       <Text style={styles.subtitle}>
         Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
         sempre que precisar.
       </Text>
 
-      <Button title=">" onPress={() => console.log('Avançar')} />
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.7}
+        onPress={() => console.log('Avançar')}
+      >
+        <Text style={styles.buttonText}>{'>'}</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   )
 }
@@ -36,7 +42,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
   },
   title: {
     fontSize: 32,
@@ -52,7 +58,19 @@ const styles = StyleSheet.create({
     color: colors.heading,
   },
   image: {
-    width: 292,
-    height: 284,
+    height: Dimensions.get('window').width * 0.7,
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 24,
   },
 })
