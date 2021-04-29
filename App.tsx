a1de288f92d6b38bd65c2580c2da8f62b7297fd9
Marcos Teodoro <marcosvinici_us@live.com>
2021-04-29 13:34:11 -0300

    Adiciona fontes do google fonts à aplicação

--- App.tsx
+++ App.tsx
@@ -1,6 +1,21 @@
 import React from 'react'
+import { Text } from 'react-native'
 import { Welcome } from './src/pages/Welcome'
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold,
+} from '@expo-google-fonts/jost'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold,
+  })
+
+  if (!fontsLoaded) {
+    return <Text>Fonte ainda não carregou</Text>
+  }
+
   return <Welcome />
 }
